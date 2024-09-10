438e45f7380458fd97203c9baf11b3ba275f4648
len <91926953+MumeiOW@users.noreply.github.com>
2024-09-10 20:18:52 +0800

    schema.sql

---
+++ schema.sql
@@ -0,0 +1,6 @@
+----Select all users who are born between January 1, 2000 and December 31, 2004.----
+select *
+from Users
+where DateOfBirth between '2000-01-01' and '2004-12-31';
+
+----Select all the posts written by the user with a user_id of 4.----
