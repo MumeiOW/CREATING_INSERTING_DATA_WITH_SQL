1d96db85aaaf597586248f86c3991c3016e14137
len <91926953+MumeiOW@users.noreply.github.com>
2024-09-10 20:30:06 +0800

    data.sql

--- data.sql
+++ data.sql
@@ -1,9 +1,9 @@
 ----Inserting Values into Users Table----
-insert into Users (UserID, Username, FirstName, LastName, DateOfBirth, Password, DateAdded) values (16, 'gjacobsen0', 'Garik', 'Jacobsen', '2002-02-17', 'opeIpjrYIrz', '2024-07-03');
-insert into Users (UserID, Username, FirstName, LastName, DateOfBirth, Password, DateAdded) values (59, 'sissacof1', 'Stafford', 'Issacof', '2003-08-23', 'rUPS8yl4', '2024-07-04');
-insert into Users (UserID, Username, FirstName, LastName, DateOfBirth, Password, DateAdded) values (14, 'mveel2', 'Melisenda', 'Veel', '2005-03-11', 'KUQj5N5L', '2023-10-11');
-insert into Users (UserID, Username, FirstName, LastName, DateOfBirth, Password, DateAdded) values (77, 'osteel3', 'Odell', 'Steel', '2004-11-03', '7zVgb1DIL4', '2024-06-14');
-insert into Users (UserID, Username, FirstName, LastName, DateOfBirth, Password, DateAdded) values (87, 'rivins4', 'Roobbie', 'Ivins', '2005-02-18', '486tK728Uu7', '2024-05-30');
+insert into Users (UserID, Username, FirstName, LastName, DateOfBirth, Password, DateAdded) values (1, 'gjacobsen0', 'Garik', 'Jacobsen', '2002-02-17', 'opeIpjrYIrz', '2024-07-03');
+insert into Users (UserID, Username, FirstName, LastName, DateOfBirth, Password, DateAdded) values (2, 'sissacof1', 'Stafford', 'Issacof', '2003-08-23', 'rUPS8yl4', '2024-07-04');
+insert into Users (UserID, Username, FirstName, LastName, DateOfBirth, Password, DateAdded) values (3, 'mveel2', 'Melisenda', 'Veel', '2005-03-11', 'KUQj5N5L', '2023-10-11');
+insert into Users (UserID, Username, FirstName, LastName, DateOfBirth, Password, DateAdded) values (4, 'osteel3', 'Odell', 'Steel', '2004-11-03', '7zVgb1DIL4', '2024-06-14');
+insert into Users (UserID, Username, FirstName, LastName, DateOfBirth, Password, DateAdded) values (5, 'rivins4', 'Roobbie', 'Ivins', '2005-02-18', '486tK728Uu7', '2024-05-30');
 
 ----Inserting Values into Friends Table----
 insert into Friends (FriendID, FriendWhoAdded, FriendBeingAdded, IsAccepted, DateAdded) values (66, 'pleitch0', 'epretsell0', 'yes', '2004-07-27');
